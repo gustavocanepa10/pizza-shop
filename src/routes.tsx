a7d0a0f97811230dd9a58dd364c25d7bcabf89b4
João <gustavociscotto@gmail.com>
2025-10-27 13:55:44 -0300

    feat : configurando rotas

---
+++ src/routes.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Dasboard } from "./pages/app/dashboard";
+import { SignIn } from "./pages/auth/sign-in";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+
+
+
+export const router = createBrowserRouter([
+
+    {path : "/", element : <AppLayout/>, 
+        children : [{path : "/", element : <Dasboard/>}]
+     },
+
+   
+    {path : "/sign-in", element : <AuthLayout/>, 
+        children : [{path : "/sign-in", element : <SignIn/>}]
+    }
+])
